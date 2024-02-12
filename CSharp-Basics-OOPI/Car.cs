20ff76b70fca28d7ffc90cd0a564a6bea2395385
Fortox <ori6g1@gmail.com>
2024-02-12 19:20:07 +0200

    added basic class

---
+++ CSharp-Basics-OOPI/Car.cs
@@ -0,0 +1,83 @@
+﻿internal class Car
+{
+    public int Acceleration;
+    public int Deceleration;
+    public int MaxSpeed;
+    
+    public int CurrentSpeed = 0;
+    public double TotalDistanceCovered = 0;
+    public bool TurnedOn = false;
+
+    public Car(int acceleration, int deceleration, int maxSpeed)
+    {
+        if (acceleration > 0)
+            Acceleration = acceleration;
+        else
+            throw new ArgumentException("Acceleration must be larger than zero.");
+
+        if (deceleration > 0)
+            Deceleration = deceleration;
+        else
+            throw new ArgumentException("Deceleration must be larger than zero.");
+
+        if ((maxSpeed % deceleration) != 0 || (maxSpeed % acceleration) != 0)
+            throw new ArgumentException("Either deceleration or acceleration are not divisors of MaxSpeed.");
+        else if (maxSpeed < 0)
+            throw new ArgumentException("MaxSpeed must be larger than zero.");
+        else
+            MaxSpeed = maxSpeed;
+    }
+
+    public void Start()
+    {
+        if (!TurnedOn)
+            TurnedOn = true;
+    }
+
+    public void TurnOff()
+    {
+        if (TurnedOn && CurrentSpeed == 0)
+            TurnedOn = false;
+    }
+
+    public void Accelerate(int seconds)
+    {
+        if (seconds <= 0 || !TurnedOn)
+            return;
+
+        int startingSpeed = CurrentSpeed;
+
+        if ((CurrentSpeed + (Acceleration * seconds)) >= MaxSpeed)
+                CurrentSpeed = MaxSpeed;
+        else
+                CurrentSpeed += Acceleration * seconds;
+
+        TotalDistanceCovered += (startingSpeed + ((CurrentSpeed - startingSpeed) / 2)) * seconds;
+    }
+
+    public void Deccelerate(int seconds)
+    {
+        if (seconds <= 0 || !TurnedOn)
+            return;
+
+        int startingSpeed = CurrentSpeed;
+
+        if ((CurrentSpeed - (Deceleration * seconds)) <= 0)
+            CurrentSpeed = 0;
+        else
+            CurrentSpeed -= Deceleration * seconds;
+
+        TotalDistanceCovered += (startingSpeed + ((CurrentSpeed - startingSpeed) / 2)) * seconds;
+    }
+
+    public void Cruise(int seconds) 
+    {
+        if (TurnedOn)
+            TotalDistanceCovered += (seconds * CurrentSpeed);
+    }
+
+    private void Drive()
+    {
+
+    }
+}
--- CSharp-Basics-OOPI/Program.cs
+++ CSharp-Basics-OOPI/Program.cs
@@ -1,3 +1,32 @@
 ﻿// ---- C# I (Dor Ben Dor) ----
 // Ori Shacham-Barr
 // ----------------------------
+
+Car car = new Car(10, 20, 160);
+
+car.Start();
+
+Console.WriteLine($"Distance: {car.TotalDistanceCovered}");
+Console.WriteLine($"Speed: {car.CurrentSpeed}\r\n");
+
+car.Accelerate(3);
+Console.WriteLine($"Distance: {car.TotalDistanceCovered}");
+Console.WriteLine($"Speed: {car.CurrentSpeed}\r\n");
+
+car.Accelerate(4);
+Console.WriteLine($"Distance: {car.TotalDistanceCovered}");
+Console.WriteLine($"Speed: {car.CurrentSpeed}\r\n");
+
+car.Cruise(3);
+Console.WriteLine($"Distance: {car.TotalDistanceCovered}");
+Console.WriteLine($"Speed: {car.CurrentSpeed}\r\n");
+
+car.Deccelerate(3);
+Console.WriteLine($"Distance: {car.TotalDistanceCovered}");
+Console.WriteLine($"Speed: {car.CurrentSpeed}\r\n");
+
+car.Deccelerate(4);
+Console.WriteLine($"Distance: {car.TotalDistanceCovered}");
+Console.WriteLine($"Speed: {car.CurrentSpeed}\r\n");
+
+car.TurnOff();
